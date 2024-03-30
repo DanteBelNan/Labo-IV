a7523f82d0183035d8b148ccee91978296804278
Teo Beltran <teo.belnan@gmail.com>
2024-03-30 17:07:01 -0300

    Constructor, get+set id

--- TP1_GRUPO_1/src/Ejercicio1/Empleado.java
+++ TP1_GRUPO_1/src/Ejercicio1/Empleado.java
@@ -5,8 +5,14 @@ public class Empleado {
 	private String nombre;
 	private int edad;
 	
+	public Empleado(int id, String nombre, int edad) {
+		this.id = id;
+		this.nombre = nombre;
+		//
+	}
+	
 	public int getId() { return id;	}
 	public void setId(int id) {	this.id = id;}
 	
-    
+    //
 }
